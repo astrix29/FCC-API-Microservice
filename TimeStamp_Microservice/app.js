07fc09c551ff012b1fa083f446f01e4b3aad79bf
Arsenic <vector2912@gmail.com>
2020-12-29 15:14:28 +0530

    Added first project

---
+++ TimeStamp_Microservice/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+// Middlewareap
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.render("./index.html");
+});
+
+app.get("/api/timestamp/:date", (req, res) => {
+	const date = req.params.date;
+
+	const utcFormat = date.includes("-")
+		? new Date(date).toUTCString()
+		: new Date(Number(date)).toUTCString();
+	const unixFormat = !date.includes("-")
+		? Number(date)
+		: new Date(date).getTime();
+
+	if (utcFormat && unixFormat) {
+		res.json({ unix: unixFormat, utc: utcFormat });
+	} else {
+		res.json({ error: "Invalid Date" });
+	}
+});
+
+app.get("/api/timestamp", (req, res) => {
+	const utcFormat = new Date().toUTCString();
+	const unixFormat = new Date().getTime();
+
+	res.json({ unix: unixFormat, utc: utcFormat });
+});
+
+app.listen(3000, () => {
+	console.log("Listening on port 3000");
+});
